651fcbc9ebf484d18d8a5ca70f8355dca4288361
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-21 14:32:56 +0530

    Subsets

--- Recursion/Subsets/java/PossibleSubsets.java
+++ Recursion/Subsets/java/PossibleSubsets.java
@@ -1,1 +1,17 @@
 package Recursion.Subsets.java;
+
+import java.util.ArrayList;
+
+public class PossibleSubsets {
+    public static void main(String[] args) {
+        ArrayList<String> result = subSets(new ArrayList<>(), "abc");
+        System.out.print(result); 
+    }
+
+    private static ArrayList<String> subSets(ArrayList<String> list, String s){
+        if(s.isEmpty()) return list;
+
+        list.add(s);
+        return subSets(list, s.substring(1));
+    }
+}
