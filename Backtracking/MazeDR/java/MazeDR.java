cfaceeea33a47220705bc625cc88135736a663ee
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-25 11:40:56 +0530

    Backtracking Maze Solver (Only Down & Right) with ArrayList and Path

--- Backtracking/MazeDR/java/MazeDR.java
+++ Backtracking/MazeDR/java/MazeDR.java
@@ -1,9 +1,10 @@
 package Backtracking.MazeDR.java;
 
+import java.util.*;
+
 public class MazeDR {
     public static  void main(String[] args) {
-        int ans = mazeDR(3, 3);
-
+        ArrayList<String> ans = mazePathListDR(new ArrayList<>(), "", 2, 2);
         System.out.println(ans);
     }   
 
@@ -17,5 +18,30 @@ public class MazeDR {
         int rightCount = mazeDR(r, c - 1);
 
         return downCount + rightCount;
+    }
+
+    public static void mazePathDR(String path, int r, int c){
+        if(r < 0 || c < 0)
+            return;
+        if(r == 0 && c == 0){
+            System.out.println(path);
+            return;
+        }
+        
+        mazePathDR(path + 'D', r - 1, c);
+        mazePathDR(path + 'R', r, c - 1);
+    }
+
+    public static ArrayList<String> mazePathListDR(ArrayList<String> list, String path, int r, int c){
+        if(r < 0 || c < 0)
+            return new ArrayList<>();
+        if(r == 0 && c == 0)
+            list.add(path);
+        
+        
+        mazePathListDR(list, path + 'D', r - 1, c);
+        mazePathListDR(list, path + 'R', r, c - 1);
+
+        return list;
     }
 }
