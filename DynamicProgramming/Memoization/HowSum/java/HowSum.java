34a68484e3b7c6572d2e0520e38f8234d7bd074e
venkatk-git <venkatkumar.profiles@gmail.com>
2024-08-01 11:38:47 +0530

    DP HowSum Memoizied

--- DynamicProgramming/Memoization/HowSum/java/HowSum.java
+++ DynamicProgramming/Memoization/HowSum/java/HowSum.java
@@ -7,25 +7,31 @@ public class HowSum {
         ArrayList<Integer> nums = new ArrayList<>();
         nums.add(7); nums.add(15);
 
-        ArrayList<Integer> result = howSum(nums, 22);
+        ArrayList<Integer> result = howSum(nums, 22, new HashMap<>());
         System.out.println(result);
     }
 
-    private static ArrayList<Integer> howSum(ArrayList<Integer> nums, int target){
-        if(target == 0) return new ArrayList<>();
+    private static ArrayList<Integer> howSum(ArrayList<Integer> nums, int target, HashMap<Integer, ArrayList<Integer>> memo){
+        if(memo.containsKey(target))
+            return memo.get(target);
+
+        if(target == 0)
+            return new ArrayList<>();
 
         if(target < 0){
           return null;  
         } 
 
         for(int num : nums){
-            ArrayList<Integer> result = howSum(nums, target - num);
+            ArrayList<Integer> result = howSum(nums, target - num, memo);
             if (result != null) {
                 result.add(num);
+                memo.put(target, result);
                 return result;
             }
         }
 
+        memo.put(target, null);
         return null;
     }
 }
