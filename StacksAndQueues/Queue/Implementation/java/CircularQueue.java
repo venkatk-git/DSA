136a93258202913164891e64daa731c9dafc7e01
venkatk-git <venkatkumar.profiles@gmail.com>
2024-08-26 17:25:18 +0530

    Fix Queue

--- StacksAndQueues/Queue/Implementation/java/CircularQueue.java
+++ StacksAndQueues/Queue/Implementation/java/CircularQueue.java
@@ -3,8 +3,8 @@ package StacksAndQueues.Queue.Implementation.java;
 public class CircularQueue {
     protected int[] queue;  
     private static final int DEFAULT_SIZE = 10;
-    private int start = 0;
-    private int end = 0;
+    // private int start = 0;
+    // private int end = 0;
 
     public CircularQueue(){
         this.queue = new int[DEFAULT_SIZE];
