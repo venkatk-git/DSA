a1982da7d065859725e8d9a2b34b3891e347a571
venkatk-git <venkatkumar.profiles@gmail.com>
2024-10-05 11:01:41 +0530

    Added Merge Intervals

--- Greedy/MergeIntervals/java/MergeIntervals.java
+++ Greedy/MergeIntervals/java/MergeIntervals.java
@@ -1,31 +1,39 @@
 package Greedy.MergeIntervals.java;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-/**
- * Merge Intervals
- */
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        ArrayList<int[]> mergedIntervals = new ArrayList<>();
+
+        Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
+
+        int currStart;
+        int currEnd;
+        for(int[] interval : intervals){
+            if(mergedIntervals.isEmpty() || mergedIntervals.getLast()[1] < interval[0]){
+                currStart = interval[0];
+                currEnd = interval[1];
+            } else {
+                currStart = mergedIntervals.getLast()[0];
+                currEnd = Math.max(mergedIntervals.getLast()[1], interval[1]);
+                mergedIntervals.removeLast();
+            }
+
+            mergedIntervals.add(new int[]{currStart, currEnd});
+        }
+
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][2]);
+    }
+}
 
 public class MergeIntervals {
     public static void main(String[] args) {
         int[][] intervals = new int[][]{{1,4},{4,5}};
-        
-        int[][] list = merge(intervals);
-        for(int[] arr : list)
+        Solution solution = new Solution();
+
+        for(int[] arr : solution.merge(intervals))
             System.out.println(Arrays.toString(arr));
-    }
-    public static int[][] merge(int[][] intervals) {
-        List<int[]> res = new ArrayList<>();
-
-        Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
-
-        for(int[] interval : intervals){
-            if(res.isEmpty() || interval[0] > res.getLast()[1])
-                res.add(interval);
-            else 
-                res.getLast()[1] = Math.max(res.getLast()[1], interval[1]);
-        }
-
-        return res.toArray(new int[res.size()][2]);
     }
 }
