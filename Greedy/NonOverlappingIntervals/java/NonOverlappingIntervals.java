9d25c54333245c39fe5b4ae2e6f279226a1116b2
venkatk-git <venkatkumar.profiles@gmail.com>
2024-10-05 11:32:39 +0530

    Revised Non Overlapping Intervals

--- Greedy/NonOverlappingIntervals/java/NonOverlappingIntervals.java
+++ Greedy/NonOverlappingIntervals/java/NonOverlappingIntervals.java
@@ -8,24 +8,24 @@ import java.util.*;
 
 public class NonOverlappingIntervals {
     public static void main(String[] args) {
-        int[][] intervals = new int[][]{{10, 13},{9,14},{7,11},{12,16},{20,25},{1,50}};
+        int[][] intervals = new int[][]{{1,2},{2,3},{3,4},{1,3}};
         
         System.out.println(eraseOverlapIntervals(intervals));
     }
+
     public static int eraseOverlapIntervals(int[][] intervals) {
         int n = intervals.length;
-        int scheduledIntervals = 0;
+        int scheduled = 0;
         int currEndTime = Integer.MIN_VALUE;
-
         Arrays.sort(intervals, (i1, i2) -> i1[1] - i2[1]);
 
         for(int i = 0; i < n; i++){
             if(intervals[i][0] >= currEndTime){
+                scheduled += 1;
                 currEndTime = intervals[i][1];
-                scheduledIntervals++;
-            }
+            }            
         }
 
-        return n - scheduledIntervals;
+        return n - scheduled;
     }
 }
