566a6d3a275fd9398870002d34d879a994bb630a
venkatk-git <venkatkumar.profiles@gmail.com>
2024-10-15 10:04:47 +0530

    Added Graph : Detect Cycle (DFS)

--- Graphs/DetectCycle/java/DetectCycle.java
+++ Graphs/DetectCycle/java/DetectCycle.java
@@ -15,12 +15,12 @@ class Pair {
 }
 
 class Solution {
-    public boolean isCycle(List<List<Integer>> adj) {
-        boolean[] visited = new boolean[adj.size()];
+    public boolean isCycle(List<List<Integer>> adj, int V) {
+        boolean[] visited = new boolean[V];
         Arrays.fill(visited, false);
 
-        for(int i = 0; i < adj.size(); i++) {
-            if(!visited[i] && bfs(adj, i, visited)){
+        for(int i = 0; i < V; i++) {
+            if(!visited[i] && dfs(adj, i, -1, visited)){
                 return true;
             }
         }
@@ -52,21 +52,35 @@ class Solution {
         return false;
     }
 
-    public void dfs(List<List<Integer>> adj) {
+    public boolean dfs(List<List<Integer>> adj, int src, int parent, boolean[] visited) {
+        visited[src] = true;
 
+        for(int v : adj.get(src)) {
+            if(!visited[v]) {
+                if(dfs(adj, v, src, visited)) {
+                    return true;
+                }
+            } else if(v != parent) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
 public class DetectCycle {
     public static void main(String[] args) {
         List<List<Integer>> adj = List.of(
-            List.of(1),
-            List.of(0, 2, 4),
-            List.of(1, 3),
-            List.of(2, 4),
-            List.of(1, 3)
+            List.of(4, 2),
+            List.of(4, 1),
+            List.of(6, 7),
+            List.of(5, 13),
+            List.of(10, 12),
+            List.of(4, 17),
+            List.of(2, 14)
         );
 
-        System.out.println(new Solution().isCycle(adj));
+        System.out.println(new Solution().isCycle(adj, 20));
     }
 }
--- Trees/DFS/java/DFS.java
+++ Trees/DFS/java/DFS.java
@@ -34,17 +34,54 @@ class BinaryTree {
             curr = curr.right;
         }
     }
+
+    public void preorder() {
+        if(root == null) {
+            return;
+        }
+
+        Stack<Node> s = new Stack<>();
+        Node curr = root;
+
+        while(curr != null || !s.isEmpty()) {
+            while(curr != null) {
+                System.out.println(curr.val);
+                
+                if(curr.right != null) {
+                    s.push(curr.right);
+                }
+
+                curr = curr.left;
+            }
+
+            if(!s.isEmpty()) {
+                curr = s.pop();
+            }
+        }
+    }
+
+    public void postorder() {
+        if(root == null) {
+            return;
+        }        
+
+        Stack<Node> s = new Stack<>();
+        Node curr = root;
+    }
 }
 
 public class DFS {
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
-        bt.root = new Node(1);
-        bt.root.left = new Node(2);
-        bt.root.right = new Node(3);
-        bt.root.left.left = new Node(4);
-        bt.root.left.right = new Node(5);
-        bt.inorder();
+        bt.root = new Node(10);
+        bt.root.left = new Node(20);
+        bt.root.right = new Node(30);
+        bt.root.left.left = new Node(40);
+        bt.root.left.left.left = new Node(70);
+        bt.root.left.right = new Node(50);
+        bt.root.right.left = new Node(60);
+        bt.root.left.left.right = new Node(80);
+        bt.preorder();
     }
     
 }
