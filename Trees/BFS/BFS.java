d6a9620a1bc47986134924ec0792af6a528f91c8
venkatk-git <venkatkumar.profiles@gmail.com>
2024-12-03 10:50:43 +0530

    Binary_Trees: Basic Breath First Search

---
+++ Trees/BFS/BFS.java
@@ -0,0 +1,63 @@
+package Trees.BFS;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> bfs(TreeNode root) {
+        List<Integer> bfs = new ArrayList<>();
+
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++) {
+                TreeNode currNode = q.poll();    
+                bfs.add(currNode.val);
+                
+                if(currNode.left != null) {
+                    q.offer(currNode.left);
+                }
+
+                if(currNode.right != null) {
+                    q.offer(currNode.right);
+                }
+            }
+        }
+
+        return bfs;
+    }
+}
+
+public class BFS {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        root.left.left.left = new TreeNode(8);
+        root.left.left.right = new TreeNode(9);
+
+        System.out.println(new Solution().bfs(root));
+    }   
+}
