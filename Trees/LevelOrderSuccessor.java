0d671f1b058ecbf8b157af1797cf0df68d2f8205
venkatk-git <venkatkumar.profiles@gmail.com>
2024-12-04 19:51:59 +0530

    Binary_Trees: Level order successor of node

---
+++ Trees/LevelOrderSuccessor.java
@@ -0,0 +1,70 @@
+package Trees;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+            return val + "";
+    }
+}
+
+class Solution {
+    public static int levelOrderSuccssor(TreeNode root, int val) {
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                int currVal = curr.val;
+
+                if(curr.left != null) {
+                    q.offer(curr.left);
+                }
+
+                if(curr.right != null) {
+                    q.offer(curr.right);
+                }
+
+                if(currVal == val) {
+                    if(q.isEmpty()) return -1;
+
+                    return q.poll().val;
+                }
+            }
+
+        }
+
+        return -1;
+    }
+}
+
+public class LevelOrderSuccessor {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(12);
+        root.left.right = new TreeNode(14);
+        root.right.left = new TreeNode(36);
+        root.right.right = new TreeNode(9);
+
+        System.out.println(new Solution().levelOrderSuccssor(root, 3));
+    }  
+}
